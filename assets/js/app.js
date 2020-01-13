2e160a8d3201347b06575abad76f666f6f8e034c
jackrapp <jacklyn.rapp@gmail.com>
2020-01-13 16:32:45 -0600

    filtered out all causes

--- assets/js/app.js
+++ assets/js/app.js
@@ -39,15 +39,19 @@ function updateLinechart(chosenState) {
     // run this to see what is in the data
     // also to make sure the data loads
     console.log(data);
+
+    var newData = data.filter(function(d) {
+      return d._113_cause_name != 'All Causes'
+    });
     
     // convert data to numerical values
-    data.forEach(function(row) {
+    newData.forEach(function(row) {
       row.year = +row.year;
       row.aadr = +row.aadr;
     });
 
     // call chart function
-    buildChart(data);
+    buildChart(newData);
   }); // end of .done(function(data))
 }; // end of updateLinechart
 
@@ -95,7 +99,6 @@ function buildChart(data) {
     .range([0, chartWidth]);
 
   // Create y scale
-  // Try d3 extent first - if weird change to d3 max value
   var yLinearScale = d3.scaleLinear()
     .domain([0, d3.max(data, d => d.aadr)])
     .range([chartHeight, 0]);
@@ -128,57 +131,4 @@ function buildChart(data) {
 
 //Build code to color circle by cause of death
 
-// Call function to make graph for default state
-  // buildLineGraph('Minnesota');
-
-// // Simply use the column’s field name as your parameter 
-//     // and the content you want to filter for as its value.
-//     // state=<state>
-//     var allCountryNames = data.state,
-//         allYear = unpack(rows, 'year'),
-//         allGdp = unpack(rows, 'gdpPercap'),
-//         listofCountries = [],
-//         currentCountry,
-//         currentGdp = [],
-//         currentYear = [];
-//     for (var i = 0; i < allCountryNames.length; i++ ){
-//       if (listofCountries.indexOf(allCountryNames[i]) === -1 ){
-//           listofCountries.push(allCountryNames[i]);
-//       }
-//   }
-    
-
-//     function setBubblePlot(chosenCountry) {
-//         getCountryData(chosenCountry);
-
-// // Pull in code for one state - MN default
-// function buildLineGraph(chosenState) {
-
-//   for (var i = 0 ; i < allCountryNames.length ; i++){
-//     if ( allCountryNames[i] === chosenCountry ) {
-//         currentGdp.push(allGdp[i]);
-//         currentYear.push(allYear[i]);
-
-//   var trace1 = {
-//     x: [1, 2, 3, 4],
-//     y: [10, 15, 13, 17],
-//     type: 'scatter',
-//     name:
-//   };
-  
-//   var trace2 = {
-//     x: [1, 2, 3, 4],
-//     y: [16, 5, 11, 9],
-//     type: 'scatter'
-//     name:
-//   };
-  
-//   var data = [trace1, trace2];
-  
-//   Plotly.newPlot('lineChart', data);
-// }
-    // Chart that displays the number of deaths over time, each type a different color
-
-    // Drop-down menu to choose state and update graph
-
-// Code to change chart size on screen change
+// Ignore data entry for all deaths __113 cause name "All Causes"
